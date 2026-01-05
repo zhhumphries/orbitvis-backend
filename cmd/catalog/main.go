97ea84e435eb56adc6d9c3aa58995987e53325e6
Zachary Humphries <zhhumphries@gmail.com>
2026-01-05 15:23:17 -0700

    Connected Catalog to DB

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
+
+var db *sql.DB
 
 func main() {
 	log.Println("Starting Catalog Service...")
@@ -23,4 +28,55 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatalf("Error starting Catalog Service: %v", err)
 	}
+}
+
+func initDB() {
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName)
+	var err error
+	db, err = sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+
+	log.Println("Successfully connected to database")
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS access_logs (
+			id SERIAL PRIMARY KEY,
+			timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`)
+	if err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if _, err := db.Exec(`
+		INSERT INTO access_logs DEFAULT VALUES
+	`); err != nil {
+		log.Printf("Failed to log access: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Count entries in access_logs
+	var count int
+	if err := db.QueryRow(`
+		SELECT COUNT(*) FROM access_logs
+	`).Scan(&count); err != nil {
+		log.Printf("Failed to count entries: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Hellow from Catalog Service! You've been here %d times.", count)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,12 @@ go 1.25.5
 
 require github.com/joshuaferrara/go-satellite v0.0.0-20220611180459-512638c64e5b
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
